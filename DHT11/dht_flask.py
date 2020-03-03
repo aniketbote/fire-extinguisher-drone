2fc4238ed3d38b90ae61f646c750b1f536bd4629
aniketbote2098 <aniketbote2098@gmail.com>
2020-03-03 12:52:13 +0530

    created dummy request file and updated req.txt

--- DHT11/dht_flask.py
+++ DHT11/dht_flask.py
@@ -8,8 +8,9 @@ temperature = []
 @app.route('/ard', methods = ['POST'])
 def dht_humid():
     content = request.get_json()
-    humidity = humidity + content['temperature']
-    print (type(content['temperature']))
+    # humidity = humidity + content['temperature']
+    # print (content['temperature'])
+    print (content)
     return 'JSON posted'
 
 app.run(host='0.0.0.0', port= 5000, debug = True)
